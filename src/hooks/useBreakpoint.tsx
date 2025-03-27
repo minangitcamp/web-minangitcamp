f1880ef314e2f14ed60648e5a2ba6f6208e722e1
masb0ymas <n.fajri@outlook.com>
2025-03-27 21:30:31 +0700

    feat: add hooks breakpoint

---
+++ src/hooks/useBreakpoint.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import useBreakpointBase from "use-breakpoint";
+
+const BREAKPOINTS = {
+  xs: 0,
+  sm: 576,
+  md: 768,
+  lg: 992,
+  xl: 1200,
+};
+
+export function useBreakPoint() {
+  return useBreakpointBase(BREAKPOINTS);
+}
